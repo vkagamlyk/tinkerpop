8bd7a32ce5799d72cad2a78bdffd70978899caf3
Florian Hockmann <fh@florian-hockmann.de>
2022-03-04 18:35:27 +0100

    TINKERPOP-2367 Add first version of a translator .NET -> Groovy

---
+++ gremlin-dotnet/src/Gremlin.Net/Process/Traversal/Translator/GroovyTranslator.cs
@@ -0,0 +1,170 @@
+﻿#region License
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#endregion
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Gremlin.Net.Process.Traversal.Translator
+{
+    /// <summary>
+    ///     Converts bytecode to a Groovy string of Gremlin.
+    /// </summary>
+    public class GroovyTranslator
+    {
+        private GroovyTranslator(string traversalSource)
+        {
+            TraversalSource = traversalSource;
+        }
+
+        /// <summary>
+        ///     Creates the translator.
+        /// </summary>
+        /// <param name="traversalSource">The traversal source for the traversal to be translated.</param>
+        /// <returns>The created translator instance.</returns>
+        public static GroovyTranslator Of(string traversalSource)
+        {
+            return new GroovyTranslator(traversalSource);
+        }
+
+        /// <summary>
+        ///     Get the language that the translator is converting the traversal byte code to.
+        /// </summary>
+        public string TargetLanguage => "gremlin-groovy";
+
+        /// <summary>
+        ///     Gets the <see cref="TraversalSource"/> representation rooting this translator.
+        ///     This is typically a "g".
+        /// </summary>
+        public string TraversalSource { get; }
+        
+        /// <summary>
+        ///     Translate <see cref="Bytecode"/> into gremlin-groovy.
+        /// </summary>
+        /// <param name="bytecode">The bytecode representing traversal source and traversal manipulations.</param>
+        /// <returns>The translated gremlin-groovy traversal.</returns>
+        public string Translate(Bytecode bytecode)
+        {
+            var sb = new StringBuilder(TraversalSource);
+            
+            foreach (var step in bytecode.SourceInstructions)
+            {
+                sb.Append(TranslateStep(step));
+            }
+            
+            foreach (var step in bytecode.StepInstructions)
+            {
+                sb.Append(TranslateStep(step));
+            }
+
+            return sb.ToString();
+        }
+
+        private string TranslateStep(Instruction step)
+        {
+            return $".{step.OperatorName}({TranslateArguments(step.Arguments)})";
+        }
+
+        private string TranslateArguments(IReadOnlyCollection<object> arguments)
+        {
+            var argumentsAsStrings = new List<string>(arguments.Count);
+
+            foreach (var argument in arguments)
+            {
+                argumentsAsStrings.Add(TranslateArgument(argument));
+            }
+
+            return string.Join(", ", argumentsAsStrings);
+        }
+
+        private string TranslateArgument(object argument)
+        {
+            if (argument == null)
+                return "null";
+            if (argument is string _)
+                return $"'{argument}'";
+            if (argument is DateTimeOffset dto)
+                return TranslateDateTimeOffset(dto);
+            if (argument is Guid guid)
+                return TranslateGuid(guid);
+            if (argument is P p)
+                return TranslateP(p);
+            if (argument is IDictionary dict)
+            {
+                return TranslateDictionary(dict);
+            }
+            if (argument is IEnumerable e)
+            {
+                return TranslateCollection(e);
+            }
+            return Convert.ToString(argument, CultureInfo.InvariantCulture);
+        }
+
+        private string TranslateDictionary(IDictionary dict)
+        {
+            var kvStrings = new List<string>(dict.Count);
+            foreach (DictionaryEntry kv in dict)
+            {
+                kvStrings.Add($"{TranslateArgument(kv.Key)}: {TranslateArgument(kv.Value)}");
+            }
+
+            return $"[{string.Join(", ", kvStrings)}]";
+        }
+
+        private bool IsDictionaryType(Type type)
+        {
+            return type
+                .GetInterfaces()
+                .Any(interfaceType => interfaceType.IsConstructedGenericType
+                                      && interfaceType.GetGenericTypeDefinition() == typeof(IEnumerable<>) 
+                                      && interfaceType.GenericTypeArguments[0] is { IsConstructedGenericType: true } genericArgument 
+                                      && genericArgument.GetGenericTypeDefinition() == typeof(KeyValuePair<,>));
+        }
+
+        private string TranslateCollection(IEnumerable enumerable) =>
+            $"[{TranslateArguments(enumerable.Cast<object>().ToArray())}]";
+
+        private string TranslateP(P p)
+        {
+            return p.Other == null
+                ? $"P.{p.OperatorName}({TranslateArgument(p.Value)})"
+                : $"P.{p.OperatorName}({TranslateArgument(p.Value)}, {TranslateArgument(p.Other)})";
+        }
+
+        private static string TranslateGuid(Guid guid) => $"UUID.fromString('{guid}')";
+
+        private static string TranslateDateTimeOffset(DateTimeOffset dto)
+        {
+            var year = dto.Year - 1900;
+            var month = dto.Month - 1;
+            var dayOfMonth = dto.Day;
+            var hour = dto.Hour;
+            var minute = dto.Minute;
+            var second = dto.Second;
+            return $"new Date({year}, {month}, {dayOfMonth}, {hour}, {minute}, {second})";
+        }
+    }
+}
---
+++ gremlin-dotnet/test/Gremlin.Net.UnitTest/Process/Traversal/Translator/GroovyTranslatorTests.cs
@@ -0,0 +1,152 @@
+﻿#region License
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using Gremlin.Net.Process.Traversal;
+using Gremlin.Net.Process.Traversal.Translator;
+using Xunit;
+
+namespace Gremlin.Net.UnitTest.Process.Traversal.Translator;
+
+public class GroovyTranslatorTests
+{
+    private readonly GraphTraversalSource _g = AnonymousTraversalSource.Traversal();
+    
+    [Fact]
+    public void ShouldTranslateStepsWithSingleArguments()
+    {
+        var translator = CreateTranslator();
+
+        var translated = translator.Translate(_g.V().Values<string>("name").Bytecode);
+        
+        Assert.Equal("g.V().values('name')", translated);
+    }
+    
+    [Fact]
+    public void ShouldTranslateStepsWithMultipleArguments()
+    {
+        var translator = CreateTranslator();
+
+        var translated = translator.Translate(_g.V().Values<string>("name", "age").Bytecode);
+        
+        Assert.Equal("g.V().values('name', 'age')", translated);
+    }
+
+    [Fact]
+    public void ShouldTranslateNullArgument()
+    {
+        AssertTranslation("null", null);
+    }
+    
+    [Theory]
+    [InlineData("3, 5", 3, 5)]
+    [InlineData("3.2, 5.1", 3.2, 5.1)]
+    public void ShouldTranslateNumericArgument(string numericGroovyString, params object[] numbers)
+    {
+        AssertTranslation(numericGroovyString, numbers);
+    }
+
+    [Fact]
+    public void ShouldTranslateDateTimeOffsetArgument()
+    {
+        AssertTranslation("new Date(122, 11, 30, 12, 0, 1)", DateTimeOffset.Parse("2022-12-30T12:00:01Z"));
+    }
+
+    [Fact]
+    public void ShouldTranslateGuid()
+    {
+        AssertTranslation("UUID.fromString('ffffffff-fd49-1e4b-0000-00000d4b8a1d')",
+            Guid.Parse("ffffffff-fd49-1e4b-0000-00000d4b8a1d"));
+    }
+
+    [Fact]
+    public void ShouldTranslateCollection()
+    {
+        AssertTranslation("['test1', 'test2']", new List<string>{"test1", "test2"});
+    }
+
+    [Fact]
+    public void ShouldTranslateDictionary()
+    {
+        var dictionary = new Dictionary<object, object>
+        {
+            { "key1", "value1" },
+            { 1, "value2" }
+        };
+        AssertTranslation("['key1': 'value1', 1: 'value2']", dictionary);
+    }
+
+    [Fact]
+    public void ShouldTranslateColumn()
+    {
+        AssertTranslation("Column.keys", Column.Keys);
+    }
+    
+    [Fact]
+    public void ShouldTranslateDirection()
+    {
+        AssertTranslation("Direction.BOTH", Direction.Both);
+    }
+    
+    [Fact]
+    public void ShouldTranslateOrder()
+    {
+        AssertTranslation("Order.desc", Order.Desc);
+    }
+    
+    [Fact]
+    public void ShouldTranslatePop()
+    {
+        AssertTranslation("Pop.last", Pop.Last);
+    }
+    
+    [Fact]
+    public void ShouldTranslateScope()
+    {
+        AssertTranslation("Scope.local", Scope.Local);
+    }
+
+    [Fact]
+    public void ShouldTranslateP()
+    {
+        AssertTranslation("P.and(P.gt(20), P.lt(30))", P.Gt(20).And(P.Lt(30)));
+    }
+
+    [Fact]
+    public void ShouldTranslatePBetween()
+    {
+        AssertTranslation("P.between([20, 30])", P.Between(20, 30));
+    }
+
+    private void AssertTranslation(string expectedTranslation, params object[] objs)
+    {
+        var translator = CreateTranslator();
+        
+        var translated = translator.Translate(_g.Inject(objs).Bytecode);
+        
+        Assert.Equal($"g.inject({expectedTranslation})", translated);
+    }
+
+    private static GroovyTranslator CreateTranslator() => GroovyTranslator.Of("g");
+}
